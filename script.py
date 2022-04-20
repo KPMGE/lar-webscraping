1093733bae0cc5d5a6ca6e6aacbaaf3f6f45f8d9
KPMGE <jesuskevin254@gmail.com>
2022-04-20 09:05:13 -0300

    :sparkles: implement reservations service

--- script.py
+++ script.py
@@ -2,6 +2,77 @@ import requests
 import time
 from bs4 import BeautifulSoup
 import json
+from datetime import date
+
+labgrad_id = 1
+current_date = '2022-04-19'
+
+class Reservation: 
+    def __init__(self, professor_name, discipline_name, date, hour):
+        self.professor_name = professor_name
+        self.discipline_name = discipline_name
+        self.date = date
+        self.hour = hour
+
+    def get_json(self):
+        return {
+            'professorName': self.professor_name,
+            'disciplineName': self.discipline_name,
+            'date': self.date,
+            'hour': self.hour
+        }
+    
+
+class Labgrad:
+    def __init__(self, id):
+        self.id = id
+
+    def __get_labgrad_reservations(self):
+        current_date = date.today()
+        response = requests.get(f'https://lar.inf.ufes.br/aula/getHorarioReservas?labgrad_id={self.id}&data_atual={current_date}').text
+        dic = json.loads(response)
+        reservations_dic = dic['dados']
+
+        reservations = []
+        for reservation in reservations_dic: 
+            professor_name = reservation['nome']
+            discipline_name = reservation['disciplina']
+            dis_date = reservation['data']
+            hour = reservation['hora']
+            reservation_obj = Reservation(professor_name, discipline_name, dis_date, hour)
+            reservations.append(reservation_obj)
+
+        return reservations
+
+    def get_reservations_as_json(self): 
+        reservations_arr = self.__get_labgrad_reservations()
+
+        json_reservations = []
+        for reservation in reservations_arr: 
+            json_reservation = reservation.get_json()
+            json_reservations.append(json_reservation)
+        
+        return json_reservations
+
+
+def save_all_reservations(): 
+    labgrad1 = Labgrad(1)
+    labgrad2 = Labgrad(2)
+    labgrad3 = Labgrad(3)
+
+    json_reservations_lab1 = labgrad1.get_reservations_as_json()
+    json_reservations_lab2 = labgrad2.get_reservations_as_json()
+    json_reservations_lab3 = labgrad3.get_reservations_as_json()
+
+    reservations_json = {
+        'labgrad1': json_reservations_lab1,
+        'labgrad2': json_reservations_lab2,
+        'labgrad3': json_reservations_lab3
+    }
+
+    with open('./lar-data/reservations.json','w',encoding = 'utf-8') as f:
+        json.dump(reservations_json, f)
+
 
 class CT:
     def __init__(self, temperature, humidity, voltage):
@@ -16,7 +87,7 @@ class CT:
             "voltage": self.voltage
         }
 
-class THT: 
+class THT:
     def __init__(self):
         json_tht_string = requests.get("https://lar.inf.ufes.br/tht/getTht").text
         self.json_tht_obj = json.loads(json_tht_string)
@@ -43,7 +114,7 @@ class THT:
         ct = CT(temp, hum, vol)
         return ct
 
-    def get_dictionary(self): 
+    def get_dictionary(self):
         ct6 = self.get_ct('ct6')
         ct7 = self.get_ct('ct7')
         ct9 = self.get_ct('ct9')
@@ -63,5 +134,6 @@ def save_lar_info():
 if __name__ == "__main__":
     while True:
         save_lar_info()
+        save_all_reservations()
         ten_minutes = 10 * 60
         time.sleep(ten_minutes)
